34c31ec7abc31a7f903dc7ead3c33b5215d86ed1
Belal-Wael <belalbatnain050@gmail.com>
2026-09-19 13:27:41 +0300

    fix:product details page error

--- Client/e-commerce-ui/src/app/products/[id]/page.tsx
+++ Client/e-commerce-ui/src/app/products/[id]/page.tsx
@@ -17,12 +17,13 @@ export const generateMetaData = async ({ params }: { params: { id: string } }) =
 async function page({ params, searchParams }: { params: Promise<{ id: string }>, searchParams: Promise<{ color: string, size: string }> }) {
 
     const { id } = await params;
+    console.log(id)
     async function getProductbyID(): Promise<ProductType | null> {
         try {
             const response = await fetch(`https://lwook-ecoomerce.vercel.app/api/productManagment/${id}`);
-            // const data = await response.json();
-            // console.log("Product data:", data);
-            return null;
+            const data = await response.json();
+            console.log("Product data:", data);
+            return data;
         } catch (error) {
             console.log("error", error);
             return null;
